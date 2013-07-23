4cb7e6b9c1fc19fc73870902f000bbf73b3d74be
Francesco Guidieri <francesco.guidieri@gmail.com>
2013-07-23 16:52:14 +0200

    AbstractResourcesListener code formatted
    Change-Id: I57b1c15ef9a3d34023d09bac37633f340d18ec16

--- plugins/org.eclipse.emf.parsley.views/src/org/eclipse/emf/parsley/views/resource/AbstractResourcesListener.java
+++ plugins/org.eclipse.emf.parsley.views/src/org/eclipse/emf/parsley/views/resource/AbstractResourcesListener.java
@@ -1,4 +1,5 @@
 package org.eclipse.emf.parsley.views.resource;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -19,98 +20,103 @@ import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.ui.PlatformUI;
 
 public abstract class AbstractResourcesListener {
- 
- private IResourceChangeListener resourceChangeListener = new IResourceChangeListener() {
-  
-  
-  public void resourceChanged(final IResourceChangeEvent event) {
-   PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
-    
-    
-    public void run() {
-     manageEvent(event);
-    }
-   });
-  }
- };
-private ResourceSet resourceSet;
- 
- 
- public AbstractResourcesListener(ResourceSet resourceSet) {
-  this.resourceSet = resourceSet;
- }
- 
- private void manageEvent(final IResourceChangeEvent event) {
-  final IResourceDelta delta = event.getDelta();
-  
-  final ResourceDeltaVisitor visitor = new ResourceDeltaVisitor();
-  try {
-   delta.accept(visitor);
-  } catch (final CoreException e) {
-   e.printStackTrace();
-  }
-  
-  final List<String> changedObjectUris = new ArrayList<String>();
-  if (!visitor.getChangedResources().isEmpty()) {
-   for (final Resource resource : visitor.getChangedResources()) {
-    final String resourceUri = EcoreUtil.getURI(resource.getContents().get(0)).toString();
-    changedObjectUris.add(resourceUri);
-   }
-  }
-  
-  resourcesChanged(changedObjectUris);
- }
- 
- public abstract void resourcesChanged(List<String> changedObjectUris);
- 
- public static void registerInterestInProteoResources(final AbstractResourcesListener proteoResourcesListener) {
-  ResourcesPlugin.getWorkspace().addResourceChangeListener(proteoResourcesListener.resourceChangeListener);
- }
- 
- public static void deregisterInterestInProteoResources(final AbstractResourcesListener proteoResourcesListener) {
-  if (proteoResourcesListener != null && proteoResourcesListener.resourceChangeListener != null) {
-   ResourcesPlugin.getWorkspace().removeResourceChangeListener(proteoResourcesListener.resourceChangeListener);
-   proteoResourcesListener.resourceChangeListener = null;
-  }
- }
- 
- private class ResourceDeltaVisitor implements IResourceDeltaVisitor {
-  
-  private final Collection<Resource> changedResources = new ArrayList<Resource>();
-  
-  
-  public boolean visit(final IResourceDelta delta) {
-   final int type = delta.getResource().getType();
-   if (type == IResource.FILE) {
-    if (delta.getKind() == IResourceDelta.CHANGED && delta.getFlags() != IResourceDelta.MARKERS) {
-     final String fullPath = delta.getFullPath().toString();
-     if (!(fullPath.contains(".git") || fullPath.endsWith(".project"))) {
-    	 final Resource resource = resourceSet.getResource(URI
-					.createPlatformResourceURI(fullPath, true), true);
-      reload(resource);
-      this.changedResources.add(resource);
-      return true;
-     }
-    }
-   }
-   return true;
-  }
-  
-  private void reload(final Resource resource) {
-   if (resource.isLoaded()) {
-    resource.unload();
-    try {
-     resource.load(Collections.EMPTY_MAP);
-    } catch (final IOException e) {
-     e.printStackTrace();
-    }
-   }
-   resourceSet.getResources().add(resource);
-  }
-  
-  public Collection<Resource> getChangedResources() {
-   return this.changedResources;
-  }
- }
- 
+
+	private IResourceChangeListener resourceChangeListener = new IResourceChangeListener() {
+
+		public void resourceChanged(final IResourceChangeEvent event) {
+			PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+
+				public void run() {
+					manageEvent(event);
+				}
+			});
+		}
+	};
+	private ResourceSet resourceSet;
+
+	public AbstractResourcesListener(ResourceSet resourceSet) {
+		this.resourceSet = resourceSet;
+	}
+
+	private void manageEvent(final IResourceChangeEvent event) {
+		final IResourceDelta delta = event.getDelta();
+
+		final ResourceDeltaVisitor visitor = new ResourceDeltaVisitor();
+		try {
+			delta.accept(visitor);
+		} catch (final CoreException e) {
+			e.printStackTrace();
+		}
+
+		final List<String> changedObjectUris = new ArrayList<String>();
+		if (!visitor.getChangedResources().isEmpty()) {
+			for (final Resource resource : visitor.getChangedResources()) {
+				final String resourceUri = EcoreUtil.getURI(
+						resource.getContents().get(0)).toString();
+				changedObjectUris.add(resourceUri);
+			}
+		}
+
+		resourcesChanged(changedObjectUris);
+	}
+
+	public abstract void resourcesChanged(List<String> changedObjectUris);
+
+	public static void registerInterestInProteoResources(
+			final AbstractResourcesListener proteoResourcesListener) {
+		ResourcesPlugin.getWorkspace().addResourceChangeListener(
+				proteoResourcesListener.resourceChangeListener);
+	}
+
+	public static void deregisterInterestInProteoResources(
+			final AbstractResourcesListener proteoResourcesListener) {
+		if (proteoResourcesListener != null
+				&& proteoResourcesListener.resourceChangeListener != null) {
+			ResourcesPlugin.getWorkspace().removeResourceChangeListener(
+					proteoResourcesListener.resourceChangeListener);
+			proteoResourcesListener.resourceChangeListener = null;
+		}
+	}
+
+	private class ResourceDeltaVisitor implements IResourceDeltaVisitor {
+
+		private final Collection<Resource> changedResources = new ArrayList<Resource>();
+
+		public boolean visit(final IResourceDelta delta) {
+			final int type = delta.getResource().getType();
+			if (type == IResource.FILE) {
+				if (delta.getKind() == IResourceDelta.CHANGED
+						&& delta.getFlags() != IResourceDelta.MARKERS) {
+					final String fullPath = delta.getFullPath().toString();
+					if (!(fullPath.contains(".git") || fullPath
+							.endsWith(".project"))) {
+						final Resource resource = resourceSet.getResource(
+								URI.createPlatformResourceURI(fullPath, true),
+								true);
+						reload(resource);
+						this.changedResources.add(resource);
+						return true;
+					}
+				}
+			}
+			return true;
+		}
+
+		private void reload(final Resource resource) {
+			if (resource.isLoaded()) {
+				resource.unload();
+				try {
+					resource.load(Collections.EMPTY_MAP);
+				} catch (final IOException e) {
+					e.printStackTrace();
+				}
+			}
+			resourceSet.getResources().add(resource);
+		}
+
+		public Collection<Resource> getChangedResources() {
+			return this.changedResources;
+		}
+	}
+
 }
