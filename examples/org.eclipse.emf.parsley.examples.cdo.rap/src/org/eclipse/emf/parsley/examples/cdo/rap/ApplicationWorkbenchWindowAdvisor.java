ae86d3af0d2dfd753bf9b21c2887958879b26f35
Vincenzo Caselli <vincenzo.caselli@rcp-vision.com>
2013-10-17 16:23:14 +0200

    Added empty RAP Workbench Application

---
+++ examples/org.eclipse.emf.parsley.examples.cdo.rap/src/org/eclipse/emf/parsley/examples/cdo/rap/ApplicationWorkbenchWindowAdvisor.java
@@ -0,0 +1,29 @@
+package org.eclipse.emf.parsley.examples.cdo.rap;
+
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.application.ActionBarAdvisor;
+import org.eclipse.ui.application.IActionBarConfigurer;
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+/**
+ * Configures the initial size and appearance of a workbench window.
+ */
+public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
+
+	public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+		super(configurer);
+	}
+
+	public ActionBarAdvisor createActionBarAdvisor(IActionBarConfigurer configurer) {
+		return new ApplicationActionBarAdvisor(configurer);
+	}
+
+	public void preWindowOpen() {
+		IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+		configurer.setInitialSize(new Point(800, 600));
+		configurer.setShowCoolBar(false);
+		configurer.setShowStatusLine(false);
+		configurer.setTitle("EMF Parsley Library RAP");
+	}
+}
---
+++ examples/org.eclipse.emf.parsley.examples.cdo.rap/src/org/eclipse/emf/parsley/examples/cdo/rap/Perspective.java
@@ -0,0 +1,15 @@
+package org.eclipse.emf.parsley.examples.cdo.rap;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+/**
+ * Configures the perspective layout. This class is contributed through the
+ * plugin.xml.
+ */
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(false);
+	}
+}
