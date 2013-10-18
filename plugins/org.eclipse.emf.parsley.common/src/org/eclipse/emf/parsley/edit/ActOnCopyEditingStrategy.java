fa13c0bc70496561b9ab15742abf84d161781543
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2013-10-18 15:37:40 +0200

    deal with notifications in the editing strategy
    Change-Id: I952798cc2e7cd6cdd126f121f8ff1726643074c8

--- plugins/org.eclipse.emf.parsley.common/src/org/eclipse/emf/parsley/edit/ActOnCopyEditingStrategy.java
+++ plugins/org.eclipse.emf.parsley.common/src/org/eclipse/emf/parsley/edit/ActOnCopyEditingStrategy.java
@@ -3,9 +3,13 @@
  */
 package org.eclipse.emf.parsley.edit;
 
+import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.InternalEObject;
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.emf.edit.provider.ViewerNotification;
 import org.eclipse.emf.parsley.util.EObjectState;
 
 import com.google.inject.Inject;
@@ -33,6 +37,7 @@ public class ActOnCopyEditingStrategy implements IEditingStrategy {
 	 */
 	public void prepare(EObject original) {
 		state = new EObjectState(original);
+		enableNotifications(original, false);
 	}
 
 	/**
@@ -45,15 +50,25 @@ public class ActOnCopyEditingStrategy implements IEditingStrategy {
 	}
 
 	public void update(EObject edited) {
+		enableNotifications(edited, true);
 		EditingDomain domain = editingDomainFinder.getEditingDomainFor(edited);
 		EditCommand editCommand = new EditCommand(
 				domain, "Edit "
 						+ edited.eClass().getName(), edited, state);
 		domain.getCommandStack().execute(editCommand);
+		edited.eNotify(new ViewerNotification(
+			new ENotificationImpl(
+				(InternalEObject) edited, Notification.SET, 
+					edited.eClass().getEAllStructuralFeatures().get(0).getFeatureID(), null, edited), 
+			edited, false, true));
 	}
 
 	public void rollback(EObject edited) {
 		state.copyStateTo(edited);
+		enableNotifications(edited, true);
 	}
 
+	protected void enableNotifications(EObject edited, boolean deliver) {
+		edited.eSetDeliver(deliver);
+	}
 }
