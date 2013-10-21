dab61a8a77f5364115d89f200270b57007845499
Vincenzo Caselli <vincenzo.caselli@rcp-vision.com>
2013-10-21 18:09:01 +0200

    Fixed perspective id and removed unused view
    Change-Id: I33ed53e988fcb4e8184aae45ad1005bce989d697

--- examples/org.eclipse.emf.parsley.examples.cdo.rcp/src/org/eclipse/emf/parsley/examples/cdo/rcp/Perspective.java
+++ examples/org.eclipse.emf.parsley.examples.cdo.rcp/src/org/eclipse/emf/parsley/examples/cdo/rcp/Perspective.java
@@ -7,6 +7,7 @@ public class Perspective implements IPerspectiveFactory {
 
 	public void createInitialLayout(IPageLayout layout) {
 		layout.setEditorAreaVisible(false);
+		layout.setFixed(true);
 	}
 
 }
