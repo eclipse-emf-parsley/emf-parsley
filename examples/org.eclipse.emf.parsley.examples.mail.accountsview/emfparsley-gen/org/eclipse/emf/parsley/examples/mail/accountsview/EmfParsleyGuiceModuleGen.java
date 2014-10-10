a516ae9de4263626f966ee03bc1d39fe507d270c
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2014-10-10 15:06:41 +0200

    444663: type bindings in the Mail example
    Change-Id: I4f302812058f5c175d1f0c287090342e1d28f242
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=444663

--- examples/org.eclipse.emf.parsley.examples.mail.accountsview/emfparsley-gen/org/eclipse/emf/parsley/examples/mail/accountsview/EmfParsleyGuiceModuleGen.java
+++ examples/org.eclipse.emf.parsley.examples.mail.accountsview/emfparsley-gen/org/eclipse/emf/parsley/examples/mail/accountsview/EmfParsleyGuiceModuleGen.java
@@ -1,8 +1,12 @@
 package org.eclipse.emf.parsley.examples.mail.accountsview;
 
 import org.eclipse.emf.parsley.EmfParsleyGuiceModule;
+import org.eclipse.emf.parsley.edit.IEditingStrategy;
+import org.eclipse.emf.parsley.edit.UndoableEditingStrategy;
+import org.eclipse.emf.parsley.examples.mail.accountsview.custom.MailEmptyResourceInitializer;
 import org.eclipse.emf.parsley.examples.mail.accountsview.edit.ui.provider.ViewerContentProviderGen;
 import org.eclipse.emf.parsley.examples.mail.accountsview.ui.provider.LabelProviderGen;
+import org.eclipse.emf.parsley.resource.EmptyResourceInitializer;
 import org.eclipse.jface.viewers.IContentProvider;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -14,6 +18,14 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 public class EmfParsleyGuiceModuleGen extends EmfParsleyGuiceModule {
   public EmfParsleyGuiceModuleGen(final AbstractUIPlugin plugin) {
     super(plugin);
+  }
+  
+  public Class<? extends EmptyResourceInitializer> bindEmptyResourceInitializer() {
+    return MailEmptyResourceInitializer.class;
+  }
+  
+  public Class<? extends IEditingStrategy> bindIEditingStrategy() {
+    return UndoableEditingStrategy.class;
   }
   
   @Override
--- examples/org.eclipse.emf.parsley.examples.mail.accountsview/src/org/eclipse/emf/parsley/examples/mail/accountsview/AccountsviewGuiceModule.java
+++ examples/org.eclipse.emf.parsley.examples.mail.accountsview/src/org/eclipse/emf/parsley/examples/mail/accountsview/AccountsviewGuiceModule.java
@@ -11,11 +11,7 @@
 package org.eclipse.emf.parsley.examples.mail.accountsview;
 
 import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
-import org.eclipse.emf.parsley.edit.IEditingStrategy;
-import org.eclipse.emf.parsley.edit.UndoableEditingStrategy;
 import org.eclipse.emf.parsley.edit.domain.GlobalAdapterFactoryEditingDomainProvider;
-import org.eclipse.emf.parsley.examples.mail.accountsview.custom.MailEmptyResourceInitializer;
-import org.eclipse.emf.parsley.resource.EmptyResourceInitializer;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 import com.google.inject.Provider;
@@ -28,17 +24,8 @@ public class AccountsviewGuiceModule extends EmfParsleyGuiceModuleGen {
 	}
 
 	@Override
-	public Class<? extends EmptyResourceInitializer> bindEmptyResourceInitializer() {
-		return MailEmptyResourceInitializer.class;
-	}
-	
-	@Override
 	public Class<? extends Provider<AdapterFactoryEditingDomain>> provideAdapterFactoryEditingDomain() {
 		return GlobalAdapterFactoryEditingDomainProvider.class;
 	}
 
-	@Override
-	public Class<? extends IEditingStrategy> bindIEditingStrategy() {
-		return UndoableEditingStrategy.class;
-	}
 }
