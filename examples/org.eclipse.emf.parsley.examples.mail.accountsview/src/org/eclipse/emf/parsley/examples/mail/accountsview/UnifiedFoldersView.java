30b7f52b8a3a63e509b558d76df3920151911c36
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2016-02-23 14:01:54 +0100

    SWTBot tests for the Mail example
    Change-Id: I71fa76c2143fb43184fec350bfdeea33e72f858e

---
+++ examples/org.eclipse.emf.parsley.examples.mail.accountsview/src/org/eclipse/emf/parsley/examples/mail/accountsview/UnifiedFoldersView.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2013 RCP Vision (http://www.rcp-vision.com) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Lorenzo Bettini - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.emf.parsley.examples.mail.accountsview;
+
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.parsley.examples.mail.accountsview.views.AccountsView;
+
+public class UnifiedFoldersView extends AccountsView {
+
+	public static final String ID = "org.eclipse.emf.parsley.examples.mail.unifiedfoldersview";
+
+	@Override
+	public void mostRecentCommandAffectsResource(Command mostRecentCommand) {
+		super.mostRecentCommandAffectsResource(mostRecentCommand);
+		// if a child changes the parent is not refreshed by default
+		getViewer().refresh();
+		getViewer().expandAll();
+	}
+}
