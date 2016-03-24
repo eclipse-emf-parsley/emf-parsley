32437edd74d75521e0ef06a5faf02caa3b3d6772
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2016-03-24 18:32:28 +0100

    Reduced number of constructors in PluginConfigurationException
    Better to provide only the one we effectively used, to avoid problems
    with API later.
    
    Change-Id: Iebd790ace11acdca8e631c7f9eda15b3d03b3a8a

--- plugins/org.eclipse.emf.parsley.runtime.common/src/org/eclipse/emf/parsley/runtime/exception/PluginConfigurationException.java
+++ plugins/org.eclipse.emf.parsley.runtime.common/src/org/eclipse/emf/parsley/runtime/exception/PluginConfigurationException.java
@@ -10,32 +10,18 @@
  *******************************************************************************/
 package org.eclipse.emf.parsley.runtime.exception;
 
-import org.eclipse.emf.parsley.runtime.ui.PluginUtil;
-
 /**
- * A {@link RuntimeException} that can be thrown during the reflective operations that are done on plugins.
- * @see {@link PluginUtil#getPlugin(org.osgi.framework.Bundle)} 
+ * A {@link RuntimeException} that can be thrown during the reflective
+ * operations that are done on plugins.
  * 
  * @author Francesco Guidieri - Initial contribution and API
  */
-public class PluginConfigurationException extends RuntimeException{
+public class PluginConfigurationException extends RuntimeException {
 
-	private static final long serialVersionUID = 1213055119380436955L;
-
-	public PluginConfigurationException() {
-		super();
-	}
+	private static final long serialVersionUID = 6353093444599316450L;
 
 	public PluginConfigurationException(String message, Throwable cause) {
 		super(message, cause);
-	}
-
-	public PluginConfigurationException(String message) {
-		super(message);
-	}
-
-	public PluginConfigurationException(Throwable cause) {
-		super(cause);
 	}
 
 }
