2ccb0e0ad850da6f64ab9bea42cd9994042c4b3e
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2016-12-23 11:47:48 +0100

    regenerated example library model code
    The ecore and the genmodel have been updated so that they respect the
    generated Java code (which had previsously been refactored through jdt)
    
    Change-Id: I52dbc1f10b628d93b3d9e950c8f5c14da7ff0138

--- examples/org.eclipse.emf.parsley.examples.library/src/org/eclipse/emf/parsley/examples/library/Library.java
+++ examples/org.eclipse.emf.parsley.examples.library/src/org/eclipse/emf/parsley/examples/library/Library.java
@@ -29,6 +29,7 @@ import org.eclipse.emf.ecore.util.FeatureMap;
  *
  * <p>
  * The following features are supported:
+ * </p>
  * <ul>
  *   <li>{@link org.eclipse.emf.parsley.examples.library.Library#getName <em>Name</em>}</li>
  *   <li>{@link org.eclipse.emf.parsley.examples.library.Library#getWriters <em>Writers</em>}</li>
@@ -40,7 +41,6 @@ import org.eclipse.emf.ecore.util.FeatureMap;
  *   <li>{@link org.eclipse.emf.parsley.examples.library.Library#getParentBranch <em>Parent Branch</em>}</li>
  *   <li>{@link org.eclipse.emf.parsley.examples.library.Library#getPeople <em>People</em>}</li>
  * </ul>
- * </p>
  *
  * @see org.eclipse.emf.parsley.examples.library.EXTLibraryPackage#getLibrary()
  * @model
@@ -49,139 +49,139 @@ import org.eclipse.emf.ecore.util.FeatureMap;
 public interface Library extends Addressable
 {
   /**
-   * Returns the value of the '<em><b>Name</b></em>' attribute.
-   * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Name</b></em>' attribute.
+	 * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @return the value of the '<em>Name</em>' attribute.
-   * @see #setName(String)
-   * @see org.eclipse.emf.parsley.examples.library.EXTLibraryPackage#getLibrary_Name()
-   * @model
-   * @generated
-   */
+	 * @return the value of the '<em>Name</em>' attribute.
+	 * @see #setName(String)
+	 * @see org.eclipse.emf.parsley.examples.library.EXTLibraryPackage#getLibrary_Name()
+	 * @model
+	 * @generated
+	 */
   String getName();
 
   /**
-   * Sets the value of the '{@link org.eclipse.emf.parsley.examples.library.Library#getName <em>Name</em>}' attribute.
-   * <!-- begin-user-doc -->
+	 * Sets the value of the '{@link org.eclipse.emf.parsley.examples.library.Library#getName <em>Name</em>}' attribute.
+	 * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @param value the new value of the '<em>Name</em>' attribute.
-   * @see #getName()
-   * @generated
-   */
+	 * @param value the new value of the '<em>Name</em>' attribute.
+	 * @see #getName()
+	 * @generated
+	 */
   void setName(String value);
 
   /**
-   * Returns the value of the '<em><b>Writers</b></em>' containment reference list.
-   * The list contents are of type {@link org.eclipse.emf.parsley.examples.library.Writer}.
-   * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Writers</b></em>' containment reference list.
+	 * The list contents are of type {@link org.eclipse.emf.parsley.examples.library.Writer}.
+	 * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @return the value of the '<em>Writers</em>' containment reference list.
-   * @see org.eclipse.emf.parsley.examples.library.EXTLibraryPackage#getLibrary_Writers()
-   * @model containment="true" transient="true" volatile="true" derived="true"
-   *        extendedMetaData="group='#people'"
-   * @generated
-   */
+	 * @return the value of the '<em>Writers</em>' containment reference list.
+	 * @see org.eclipse.emf.parsley.examples.library.EXTLibraryPackage#getLibrary_Writers()
+	 * @model containment="true" transient="true" volatile="true" derived="true"
+	 *        extendedMetaData="group='#people'"
+	 * @generated
+	 */
   EList<Writer> getWriters();
 
   /**
-   * Returns the value of the '<em><b>Employees</b></em>' containment reference list.
-   * The list contents are of type {@link org.eclipse.emf.parsley.examples.library.Employee}.
-   * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Employees</b></em>' containment reference list.
+	 * The list contents are of type {@link org.eclipse.emf.parsley.examples.library.Employee}.
+	 * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @return the value of the '<em>Employees</em>' containment reference list.
-   * @see org.eclipse.emf.parsley.examples.library.EXTLibraryPackage#getLibrary_Employees()
-   * @model containment="true" transient="true" volatile="true" derived="true"
-   *        extendedMetaData="group='#people'"
-   * @generated
-   */
+	 * @return the value of the '<em>Employees</em>' containment reference list.
+	 * @see org.eclipse.emf.parsley.examples.library.EXTLibraryPackage#getLibrary_Employees()
+	 * @model containment="true" transient="true" volatile="true" derived="true"
+	 *        extendedMetaData="group='#people'"
+	 * @generated
+	 */
   EList<Employee> getEmployees();
 
   /**
-   * Returns the value of the '<em><b>Borrowers</b></em>' containment reference list.
-   * The list contents are of type {@link org.eclipse.emf.parsley.examples.library.Borrower}.
-   * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Borrowers</b></em>' containment reference list.
+	 * The list contents are of type {@link org.eclipse.emf.parsley.examples.library.Borrower}.
+	 * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @return the value of the '<em>Borrowers</em>' containment reference list.
-   * @see org.eclipse.emf.parsley.examples.library.EXTLibraryPackage#getLibrary_Borrowers()
-   * @model containment="true" transient="true" volatile="true" derived="true"
-   *        extendedMetaData="group='#people'"
-   * @generated
-   */
+	 * @return the value of the '<em>Borrowers</em>' containment reference list.
+	 * @see org.eclipse.emf.parsley.examples.library.EXTLibraryPackage#getLibrary_Borrowers()
+	 * @model containment="true" transient="true" volatile="true" derived="true"
+	 *        extendedMetaData="group='#people'"
+	 * @generated
+	 */
   EList<Borrower> getBorrowers();
 
   /**
-   * Returns the value of the '<em><b>Stock</b></em>' containment reference list.
-   * The list contents are of type {@link org.eclipse.emf.parsley.examples.library.Item}.
-   * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Stock</b></em>' containment reference list.
+	 * The list contents are of type {@link org.eclipse.emf.parsley.examples.library.Item}.
+	 * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @return the value of the '<em>Stock</em>' containment reference list.
-   * @see org.eclipse.emf.parsley.examples.library.EXTLibraryPackage#getLibrary_Stock()
-   * @model containment="true" ordered="false"
-   * @generated
-   */
+	 * @return the value of the '<em>Stock</em>' containment reference list.
+	 * @see org.eclipse.emf.parsley.examples.library.EXTLibraryPackage#getLibrary_Stock()
+	 * @model containment="true" ordered="false"
+	 * @generated
+	 */
   EList<Item> getStock();
 
   /**
-   * Returns the value of the '<em><b>Books</b></em>' reference list.
-   * The list contents are of type {@link org.eclipse.emf.parsley.examples.library.Book}.
-   * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Books</b></em>' reference list.
+	 * The list contents are of type {@link org.eclipse.emf.parsley.examples.library.Book}.
+	 * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @return the value of the '<em>Books</em>' reference list.
-   * @see org.eclipse.emf.parsley.examples.library.EXTLibraryPackage#getLibrary_Books()
-   * @model transient="true" derived="true" ordered="false"
-   * @generated
-   */
+	 * @return the value of the '<em>Books</em>' reference list.
+	 * @see org.eclipse.emf.parsley.examples.library.EXTLibraryPackage#getLibrary_Books()
+	 * @model transient="true" derived="true" ordered="false"
+	 * @generated
+	 */
   EList<Book> getBooks();
 
   /**
-   * Returns the value of the '<em><b>Branches</b></em>' containment reference list.
-   * The list contents are of type {@link org.eclipse.emf.parsley.examples.library.Library}.
-   * It is bidirectional and its opposite is '{@link org.eclipse.emf.parsley.examples.library.Library#getParentBranch <em>Parent Branch</em>}'.
-   * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Branches</b></em>' containment reference list.
+	 * The list contents are of type {@link org.eclipse.emf.parsley.examples.library.Library}.
+	 * It is bidirectional and its opposite is '{@link org.eclipse.emf.parsley.examples.library.Library#getParentBranch <em>Parent Branch</em>}'.
+	 * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @return the value of the '<em>Branches</em>' containment reference list.
-   * @see org.eclipse.emf.parsley.examples.library.EXTLibraryPackage#getLibrary_Branches()
-   * @see org.eclipse.emf.parsley.examples.library.Library#getParentBranch
-   * @model opposite="parentBranch" containment="true" resolveProxies="true"
-   * @generated
-   */
+	 * @return the value of the '<em>Branches</em>' containment reference list.
+	 * @see org.eclipse.emf.parsley.examples.library.EXTLibraryPackage#getLibrary_Branches()
+	 * @see org.eclipse.emf.parsley.examples.library.Library#getParentBranch
+	 * @model opposite="parentBranch" containment="true" resolveProxies="true"
+	 * @generated
+	 */
   EList<Library> getBranches();
 
   /**
-   * Returns the value of the '<em><b>Parent Branch</b></em>' container reference.
-   * It is bidirectional and its opposite is '{@link org.eclipse.emf.parsley.examples.library.Library#getBranches <em>Branches</em>}'.
-   * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Parent Branch</b></em>' container reference.
+	 * It is bidirectional and its opposite is '{@link org.eclipse.emf.parsley.examples.library.Library#getBranches <em>Branches</em>}'.
+	 * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @return the value of the '<em>Parent Branch</em>' container reference.
-   * @see #setParentBranch(Library)
-   * @see org.eclipse.emf.parsley.examples.library.EXTLibraryPackage#getLibrary_ParentBranch()
-   * @see org.eclipse.emf.parsley.examples.library.Library#getBranches
-   * @model opposite="branches" transient="false"
-   * @generated
-   */
+	 * @return the value of the '<em>Parent Branch</em>' container reference.
+	 * @see #setParentBranch(Library)
+	 * @see org.eclipse.emf.parsley.examples.library.EXTLibraryPackage#getLibrary_ParentBranch()
+	 * @see org.eclipse.emf.parsley.examples.library.Library#getBranches
+	 * @model opposite="branches" transient="false"
+	 * @generated
+	 */
   Library getParentBranch();
 
   /**
-   * Sets the value of the '{@link org.eclipse.emf.parsley.examples.library.Library#getParentBranch <em>Parent Branch</em>}' container reference.
-   * <!-- begin-user-doc -->
+	 * Sets the value of the '{@link org.eclipse.emf.parsley.examples.library.Library#getParentBranch <em>Parent Branch</em>}' container reference.
+	 * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @param value the new value of the '<em>Parent Branch</em>' container reference.
-   * @see #getParentBranch()
-   * @generated
-   */
+	 * @param value the new value of the '<em>Parent Branch</em>' container reference.
+	 * @see #getParentBranch()
+	 * @generated
+	 */
   void setParentBranch(Library value);
 
   /**
-   * Returns the value of the '<em><b>People</b></em>' attribute list.
-   * The list contents are of type {@link org.eclipse.emf.ecore.util.FeatureMap.Entry}.
-   * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>People</b></em>' attribute list.
+	 * The list contents are of type {@link org.eclipse.emf.ecore.util.FeatureMap.Entry}.
+	 * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @return the value of the '<em>People</em>' attribute list.
-   * @see org.eclipse.emf.parsley.examples.library.EXTLibraryPackage#getLibrary_People()
-   * @model dataType="org.eclipse.emf.ecore.EFeatureMapEntry" many="true"
-   *        extendedMetaData="kind='group'"
-   * @generated
-   */
+	 * @return the value of the '<em>People</em>' attribute list.
+	 * @see org.eclipse.emf.parsley.examples.library.EXTLibraryPackage#getLibrary_People()
+	 * @model dataType="org.eclipse.emf.ecore.EFeatureMapEntry" many="true"
+	 *        extendedMetaData="kind='group'"
+	 * @generated
+	 */
   FeatureMap getPeople();
 
 } // Library
