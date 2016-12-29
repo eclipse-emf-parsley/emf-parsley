7ce0a83b3ce499dd74ce7e80e6af43b4d0b7c1a6
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2016-12-29 15:05:43 +0100

    509684: Use old version of EditUIUtil.getURI
    The other one was introduced in EMF 2.11
    
    Change-Id: I7e699373d9715c8cf01ab01b259246a30fa11d22
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=509684

--- examples/org.eclipse.emf.parsley.examples.library.editor/src/org/eclipse/emf/parsley/examples/library/presentation/EXTLibraryEditor.java
+++ examples/org.eclipse.emf.parsley.examples.library.editor/src/org/eclipse/emf/parsley/examples/library/presentation/EXTLibraryEditor.java
@@ -937,7 +937,7 @@ public class EXTLibraryEditor extends MultiPageEditorPart
 	 */
   public void createModel()
   {
-		URI resourceURI = EditUIUtil.getURI(getEditorInput(), editingDomain.getResourceSet().getURIConverter());
+		URI resourceURI = EditUIUtil.getURI(getEditorInput());
 		Exception exception = null;
 		Resource resource = null;
 		try {
